42cc1aabe4584e183608bbd36cccf3d95c97eee0
Gilbert Kondani <83396186+Techgenius-Systems@users.noreply.github.com>
2025-04-09 17:25:28 +0100

    adding correct booking class

--- src/main/java/com/stacs/cs5031/p3/server/model/Booking.java
+++ src/main/java/com/stacs/cs5031/p3/server/model/Booking.java
@@ -1,44 +1,37 @@
-
 package com.stacs.cs5031.p3.server.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
 /**
- * Represents a booking/event in the room booking system.
- * Contains information about the event, its location, timing, organizer, and attendees.
+ * Entity representing a booking in the system.
+ * Contains information about the event, room, time, organiser, and attendees.
  */
 @Entity
 public class Booking {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
     
     private String eventName;
     
     @ManyToOne
-    @JoinColumn(name = "room_id")
     private Room room;
     
     @Temporal(TemporalType.TIMESTAMP)
     private Date startTime;
     
-    private int duration;  // duration in minutes
+    private int duration; // in minutes
     
     @ManyToOne
-    @JoinColumn(name = "organiser_id")
     private Organiser organiser;
     
     @ManyToMany
-    @JoinTable(
-        name = "booking_attendees",
-        joinColumns = @JoinColumn(name = "booking_id"),
-        inverseJoinColumns = @JoinColumn(name = "attendee_id")
-    )
     private List<Attendee> attendees = new ArrayList<>();
     
     /**
@@ -53,8 +46,8 @@ public class Booking {
      * 
      * @param eventName Name of the event
      * @param room Room where the event will take place
-     * @param startTime Start time and date of the event
-     * @param duration Duration of the event in minutes
+     * @param startTime Start time of the event
+     * @param duration Duration in minutes
      * @param organiser User who organized the event
      */
     public Booking(String eventName, Room room, Date startTime, int duration, Organiser organiser) {
@@ -63,6 +56,114 @@ public class Booking {
         this.startTime = startTime;
         this.duration = duration;
         this.organiser = organiser;
+    }
+    
+    /**
+     * Gets the booking ID.
+     * 
+     * @return The booking ID
+     */
+    public int getId() {
+        return id;
+    }
+    
+    /**
+     * Gets the event name.
+     * 
+     * @return The event name
+     */
+    public String getName() {
+        return eventName;
+    }
+    
+    /**
+     * Sets a new event name.
+     * 
+     * @param eventName The new event name
+     */
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    
+    /**
+     * Gets the room where the event will take place.
+     * 
+     * @return The room
+     */
+    public Room getRoom() {
+        return room;
+    }
+    
+    /**
+     * Sets a new room for the event.
+     * 
+     * @param room The new room
+     */
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+    
+    /**
+     * Gets the start time of the event.
+     * 
+     * @return The start time
+     */
+    public Date getStartTime() {
+        return startTime;
+    }
+    
+    /**
+     * Sets a new start time for the event.
+     * 
+     * @param startTime The new start time
+     */
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+    
+    /**
+     * Gets the duration of the event in minutes.
+     * 
+     * @return The duration in minutes
+     */
+    public int getDuration() {
+        return duration;
+    }
+    
+    /**
+     * Sets a new duration for the event.
+     * 
+     * @param duration The new duration in minutes
+     */
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    /**
+     * Gets the organiser of the event.
+     * 
+     * @return The organiser
+     */
+    public Organiser getOrganiser() {
+        return organiser;
+    }
+    
+    /**
+     * Sets a new organiser for the event.
+     * 
+     * @param organiser The new organiser
+     */
+    public void setOrganiser(Organiser organiser) {
+        this.organiser = organiser;
+    }
+    
+    /**
+     * Gets the list of attendees for this event.
+     * 
+     * @return The list of attendees
+     */
+    public List<Attendee> getAttendees() {
+        return attendees;
     }
     
     /**
@@ -81,10 +182,15 @@ public class Booking {
      * @return true if the attendee was added successfully, false if there was no space
      */
     public boolean addAttendee(Attendee attendee) {
+        if (attendees.contains(attendee)) {
+            return false; // Already registered
+        }
+        
         if (isThereSpace()) {
             attendees.add(attendee);
             return true;
         }
+        
         return false;
     }
     
@@ -99,115 +205,15 @@ public class Booking {
     }
     
     /**
-     * Gets the room where the event will take place.
-     * 
-     * @return The room object
-     */
-    public Room getRoom() {
-        return room;
-    }
-    
-    /**
-     * Gets the start time of the event.
-     * 
-     * @return The start time as a Date object
-     */
-    public Date getStartTime() {
-        return startTime;
-    }
-    
-    /**
-     * Gets the duration of the event in minutes.
-     * 
-     * @return The duration in minutes
-     */
-    public int getDuration() {
-        return duration;
-    }
-    
-    /**
-     * Gets the organiser of the event.
-     * 
-     * @return The organiser object
-     */
-    public Organiser getOrganiser() {
-        return organiser;
-    }
-    
-    /**
-     * Gets the name of the event.
-     * 
-     * @return The event name
-     */
-    public String getName() {
-        return eventName;
-    }
-    
-    /**
-     * Gets the list of attendees for this event.
-     * 
-     * @return A List of Attendee objects
-     */
-    public List<Attendee> getAttendees() {
-        return attendees;
-    }
-    
-    /**
-     * Gets the ID of the booking.
-     * 
-     * @return The booking ID
-     */
-    public Long getId() {
-        return id;
-    }
-    
-    /**
-     * Sets a new name for the event.
-     * 
-     * @param eventName The new event name
-     */
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-    
-    /**
-     * Changes the room for the event.
-     * 
-     * @param room The new room
-     */
-    public void setRoom(Room room) {
-        this.room = room;
-    }
-    
-    /**
-     * Changes the start time of the event.
-     * 
-     * @param startTime The new start time
-     */
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-    
-    /**
-     * Changes the duration of the event.
-     * 
-     * @param duration The new duration in minutes
-     */
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-    
-    /**
      * Calculates the end time of the event based on start time and duration.
      * 
-     * @return The end time as a Date object
+     * @return The end time
      */
     public Date getEndTime() {
-        // Create a new Date object to avoid modifying the original
-        Date endTime = new Date(startTime.getTime());
-        // Add duration in milliseconds
-        endTime.setTime(endTime.getTime() + (duration * 60 * 1000));
-        return endTime;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startTime);
+        calendar.add(Calendar.MINUTE, duration);
+        return calendar.getTime();
     }
     
     /**
@@ -229,12 +235,30 @@ public class Booking {
         return (this.startTime.before(otherEnd) && thisEnd.after(other.startTime));
     }
     
+    /**
+     * Gets the current number of attendees.
+     * 
+     * @return The number of attendees
+     */
+    public int getCurrentAttendeeCount() {
+        return attendees.size();
+    }
+    
+    /**
+     * Gets the maximum capacity for the booking (from the room).
+     * 
+     * @return The maximum capacity
+     */
+    public int getMaxCapacity() {
+        return room.getCapacity();
+    }
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Booking booking = (Booking) o;
-        return Objects.equals(id, booking.id);
+        return id == booking.id;
     }
     
     @Override
@@ -244,8 +268,13 @@ public class Booking {
     
     @Override
     public String toString() {
-        return "Booking [id=" + id + ", eventName=" + eventName + ", room=" + room.getName() 
-                + ", startTime=" + startTime + ", duration=" + duration + " mins, organiser=" 
-                + organiser.getUsername() + ", attendees=" + attendees.size() + "]";
+        return "Booking [id=" + id + 
+               ", eventName=" + eventName + 
+               ", room=" + (room != null ? room.getName() : "null") + 
+               ", startTime=" + startTime + 
+               ", duration=" + duration + " mins" + 
+               ", organiser=" + (organiser != null ? organiser.getUsername() : "null") + 
+               ", attendees=" + (attendees != null ? attendees.size() : 0) + 
+               "]";
     }
 }
