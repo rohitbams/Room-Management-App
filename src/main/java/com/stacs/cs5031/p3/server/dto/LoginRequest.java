2ce6c9b4bbc5c97385a272320ac37f3f84b3932b
rohit <143954072+rohitbams@users.noreply.github.com>
2025-04-12 15:24:08 +0100

    add dto model for LoginRequest

---
+++ src/main/java/com/stacs/cs5031/p3/server/dto/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.stacs.cs5031.p3.server.dto;
+
+/**
+ * LoginRequest class.
+ * This is a simple request model that implements
+ */
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
