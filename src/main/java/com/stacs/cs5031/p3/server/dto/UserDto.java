2b9846d7c160ce0916ee65b2eb329012eead87fe
rohit <143954072+rohitbams@users.noreply.github.com>
2025-04-12 13:04:06 +0100

    add UserDto class

---
+++ src/main/java/com/stacs/cs5031/p3/server/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.stacs.cs5031.p3.server.dto;
+
+public class UserDto {
+    private final Integer id;
+    private final String username;
+    private final String name;
+
+    public UserDto(Integer id, String username, String name) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
