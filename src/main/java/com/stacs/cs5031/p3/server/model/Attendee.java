121834544a0dd345187c30f70e1bb51a4f3ac8fd
rohit <143954072+rohitbams@users.noreply.github.com>
2025-04-14 10:27:53 +0100

    implement Attendee constructor

--- src/main/java/com/stacs/cs5031/p3/server/model/Attendee.java
+++ src/main/java/com/stacs/cs5031/p3/server/model/Attendee.java
@@ -1,5 +1,33 @@
 package com.stacs.cs5031.p3.server.model;
 
-public class Attendee {
-    
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Attendee model class.
+ * This is a subclass of User and represents an Attendee in the system.
+ * Attendees can register for bookings created by Organisers.
+ */
+@Entity
+public class Attendee extends User {
+
+    @ManyToMany
+    @JoinTable(
+            name = "attendee_bookings",
+            joinColumns = @JoinColumn(name = "attendee_id"),
+            inverseJoinColumns = @JoinColumn(name = "booking_id")
+    )
+    private List<Booking> registeredBookings = new ArrayList<>();
+
+    protected Attendee() {
+        super();
+    }
+
+    public Attendee(String name, String username, String password) {
+        super(name, username, password);
+    }
+
+
+
 }
--- src/test/java/com/stacs/cs5031/p3/server/model/AttendeeTest.java
+++ src/test/java/com/stacs/cs5031/p3/server/model/AttendeeTest.java
@@ -1,4 +1,37 @@
 package com.stacs.cs5031.p3.server.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AttendeeTest {
+
+    private Attendee attendee;
+    private Booking booking;
+    private Date startTime;
+
+    @BeforeEach
+    void setup() {
+        attendee = new Attendee("John Doe", "johndoe", "password123");
+
+        // mock booking
+        Room room = new Room("name ", 1);
+        Organiser organiser = new Organiser("organiser_name", "organiser", "password");
+        startTime = new Date();
+        booking = new Booking("event_name", room, startTime,60, organiser);
+    }
+
+    @Test
+    void shouldCreateAttendee() {
+        assertNotNull(attendee, "attendee should not be null");
+        assertEquals("John Doe", attendee.getName(), "name should match");
+        assertEquals("johndoe", attendee.getUsername(), "username should match");
+        assertEquals("password123", attendee.getPassword(), "password should match");
+    }
+
+
 }
