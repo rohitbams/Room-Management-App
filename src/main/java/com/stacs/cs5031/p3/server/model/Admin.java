45e8ee38b91011cbbc70372a4290a6aee2168aab
j <jv@st-andrews.ac.uk>
2025-04-14 14:23:35 +0100

    Add skeleton method implementation in Admin and failing tests in AdminTest

---
+++ src/main/java/com/stacs/cs5031/p3/server/model/Admin.java
@@ -0,0 +1,58 @@
+package com.stacs.cs5031.p3.server.model;
+
+import java.util.ArrayList;
+
+import com.stacs.cs5031.p3.server.dto.RoomDto;
+import com.stacs.cs5031.p3.server.service.OrganiserService;
+import com.stacs.cs5031.p3.server.service.RoomService;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin extends User {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String username;
+    private String password;
+    private RoomService roomService; // Dependency for managing rooms
+    private OrganiserService organiserService; // Dependency for managing organisers
+    // private AttendeeService attendeeService;
+
+    public Admin() {
+    }
+    
+    //TODO add attendeeservice field
+    public Admin(String name, String username, String password, RoomService roomService, OrganiserService organiserService) {
+        super(name, username, password);
+        this.roomService = roomService;
+        this.organiserService = organiserService;
+        // this.attendeeService = attendeeService;
+    }
+
+    public ArrayList<RoomDto> getAllRooms() {
+        return new ArrayList<>(roomService.findAllRooms());
+    }
+    
+    //TODO
+    // public ArrayList<Attendee> getAttendees() {
+    //     return new ArrayList<>();
+    // }
+
+    public ArrayList<Organiser> getOrganisers() {
+        return new ArrayList<>();
+    }
+
+    public boolean addRoom(Room room) {
+        return false;
+    }
+
+    public boolean removeRoom(int roomId) {
+        return false;
+    }
+}
