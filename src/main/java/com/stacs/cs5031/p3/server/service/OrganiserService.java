e9f60850ed8e9bc8c36c5711aac3fa200e06303a
n <nd@st-andrews.ac.uk>
2025-04-16 21:30:18 +0100

    finished the organiser controller - still need to refine tests

--- src/main/java/com/stacs/cs5031/p3/server/service/OrganiserService.java
+++ src/main/java/com/stacs/cs5031/p3/server/service/OrganiserService.java
@@ -2,6 +2,7 @@ package com.stacs.cs5031.p3.server.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.stacs.cs5031.p3.server.model.Booking;
 import org.springframework.stereotype.Service;
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service;
 import com.stacs.cs5031.p3.server.dto.BookingDto;
 import com.stacs.cs5031.p3.server.dto.OrganiserDto;
 import com.stacs.cs5031.p3.server.dto.RoomDto;
-import com.stacs.cs5031.p3.server.model.Room;
+import com.stacs.cs5031.p3.server.exception.BookingNotFoundException;
+import com.stacs.cs5031.p3.server.mapper.BookingDtoMapper;
 import com.stacs.cs5031.p3.server.model.Organiser;
 import com.stacs.cs5031.p3.server.repository.OrganiserRepository;
 
 /**
  * This class is the service layer for the Organiser entity.
+ * 
  * @author 190031593
  */
 @Service
@@ -26,11 +29,13 @@ public class OrganiserService {
 
     /**
      * Constructor for the OrganiserService class.
+     * 
      * @param organiserRepository - organiser repository.
-     * @param roomService - room service.
-     * @param bookingService - booking service.
+     * @param roomService         - room service.
+     * @param bookingService      - booking service.
      */
-    public OrganiserService(OrganiserRepository organiserRepository, RoomService roomService, BookingService bookingService) {
+    public OrganiserService(OrganiserRepository organiserRepository, RoomService roomService,
+            BookingService bookingService) {
         this.organiserRepository = organiserRepository;
         this.roomService = roomService;
         this.bookingService = bookingService;
@@ -38,6 +43,7 @@ public class OrganiserService {
 
     /**
      * This method is used to create an organiser and save it to the database.
+     * 
      * @param organiser - The organiser to be created.
      * @return String - The status of the operation.
      * @throws IllegalArgumentException - If the organiser credentials are invalid.
@@ -49,7 +55,7 @@ public class OrganiserService {
 
         String name = organiser.getName();
         String username = organiser.getUsername();
-        String password = organiser.getPassword(); 
+        String password = organiser.getPassword();
 
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Organiser name is invalid");
@@ -67,78 +73,113 @@ public class OrganiserService {
 
     /***
      * This method is used to get all the organisers from the database.
+     * 
      * @return ArrayList<OrganiserDto> - List of all the organisers.
      */
     public ArrayList<OrganiserDto> getAllOrganisers() {
         ArrayList<Organiser> organisers = new ArrayList<Organiser>(organiserRepository.findAll());
         ArrayList<OrganiserDto> organiserDtos = new ArrayList<OrganiserDto>();
         organisers.forEach(organiser -> {
-            OrganiserDto organiserDto = new OrganiserDto(organiser.getId(), organiser.getName(), organiser.getUsername());
+            OrganiserDto organiserDto = new OrganiserDto(organiser.getId(), organiser.getName(),
+                    organiser.getUsername());
             organiserDtos.add(organiserDto);
         });
 
         return organiserDtos;
     }
-    
 
     /**
      * This method is used to get all the available rooms from the database.
+     * 
      * @return ArrayList<RoomDto> - List of all the available rooms.
      */
     public List<RoomDto> getAvailableRooms() {
         return roomService.findAvailableRooms();
     }
 
-     public ArrayList<String> getMyBookings() {
-         return new ArrayList<String>();
-     }
+    /**
+     * This method is used to get all the bookings for an organiser.
+     * @param organiserId - The ID of the organiser
+     * @return ArrayList<BookingDto> - List of all the bookings for the organiser
+     */
+    public ArrayList<BookingDto> getBookings(int organiserId) {
+        Long organiserIdAsLong = Long.valueOf(organiserId); // Replace with actual organiser ID
 
-     public ArrayList<Booking> getMyBooking(int bookingId) {
-         return new ArrayList<Booking>();
-     }
+        ArrayList<Booking> bookings = (ArrayList<Booking>) bookingService.getBookingsByOrganiser(organiserIdAsLong);
 
-     /**
-      * This method is used to cancel a booking.
-      * @param bookingId - The ID of the booking to be cancelled
-      * @return String - String stating whether or not the booking was cancelled successfully
-      */
-     public String cancelBooking(int bookingId, int organiserId) {
-        Long bookingIdAsLong = Long.valueOf(bookingId);
-        Long organiserIdAsLong = Long.valueOf(organiserId);
-        try{
-            ArrayList<Booking> organiserBookings = (ArrayList<Booking>) bookingService.getBookingsByOrganiser(organiserIdAsLong);
-            if (organiserBookings.isEmpty()) {
-                return "No bookings found for this organiser.";
-            }
+        ArrayList<BookingDto> bookingDtos = new ArrayList<BookingDto>();
+        bookings.forEach(booking -> {
+            BookingDto bookingDto = BookingDtoMapper.mapToDTO(booking);
+            bookingDtos.add(bookingDto);
+        });
 
-            boolean found = false;
-            for (Booking booking: organiserBookings){
-                if (booking.getId() == bookingIdAsLong) {
-                    bookingService.deleteBooking(bookingIdAsLong);
-                    found = true;
-                    return "SUCCESS!";
-                }
-            };
-            return !found ? "Booking not found for this organiser." : "FAILURE!";
+        return bookingDtos;
         
-        } catch (IllegalArgumentException e) {
-            return "FAILURE!";
+    }
+
+    /**
+     * This method is used to get a booking by its ID.
+     * @param bookingId - The ID of the booking
+     * @param organiserId - The ID of the organiser
+     * @return BookingDto - The booking DTO
+     * @throws BookingNotFoundException - If the booking is not found
+     */
+    public BookingDto getBooking(int bookingId, int organiserId) {
+        Long bookingIdAsLong = Long.valueOf(bookingId);
+        
+        Optional<Booking> organiserBooking = bookingService.getBookingById(bookingIdAsLong);
+
+        if(!organiserBooking.isPresent()) {
+            throw new BookingNotFoundException(bookingId);
+        }else{
+            Booking booking = organiserBooking.get();
+            if (booking.getOrganiser().getId() != organiserId) {
+                return null;
+            }else{
+                return BookingDtoMapper.mapToDTO(booking);
+            }
         }
-     }
+    }
 
-     /***
-      * This method is used to create a booking for an event.
-      * @param booking - The booking to be created
-      * @param organiserId - The ID of the organiser
-      * @return String - String stating whether or not the booking was successful
-      */
-     public String createBooking(BookingDto.BookingRequest booking, int organiserId) {
+    /**
+     * This method is used to cancel a booking.
+     * 
+     * @param bookingId - The ID of the booking to be cancelled
+     * @return String - String stating whether or not the booking was cancelled
+     *         successfully
+     */
+    public String cancelBooking(int bookingId, int organiserId) {
+        Long bookingIdAsLong = Long.valueOf(bookingId);
 
-        try{
+        Optional<Booking> organiserBooking = bookingService.getBookingById(bookingIdAsLong);
+
+        if(!organiserBooking.isPresent()) {
+            throw new BookingNotFoundException(bookingId);
+        }else{
+            Booking booking = organiserBooking.get();
+            if (booking.getOrganiser().getId() != organiserId) {
+                return "You are not the organiser of this booking";
+            }else{
+                bookingService.deleteBooking(bookingIdAsLong);
+                return "SUCCESS!";
+            }
+        }
+    }
+
+    /***
+     * This method is used to create a booking for an event.
+     * 
+     * @param booking     - The booking to be created
+     * @param organiserId - The ID of the organiser
+     * @return String - String stating whether or not the booking was successful
+     */
+    public String createBooking(BookingDto.BookingRequest booking, int organiserId) {
+
+        try {
             bookingService.createBooking(booking, Long.valueOf(organiserId));
-        }catch(Exception e){
+        } catch (Exception e) {
             return e.getMessage();
         }
-         return "SUCCESS!";
-     }
+        return "SUCCESS!";
+    }
 }
